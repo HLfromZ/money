3cf8b5b964a2e3f519836e0e2f479721907ebd62
HLfromZ <hl.1012@icloud.com>
2026-03-10 15:53:20 +0800

    feat: 引入tokio，支持异步main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use money::config::env::Config;
 use money::config::log;
 use tracing::info;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     Config::init();
     let config = Config::get();
     let _log_guard = log::init_log(&config.log_level);
