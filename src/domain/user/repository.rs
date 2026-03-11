2b532a0fd160bcd3ee8f0ac4ef7f597c195ea499
HLfromZ <hl.1012@icloud.com>
2026-03-11 14:32:38 +0800

    feat: user
    1. user 建表
    2. user 实体、数据库操作逻辑
    
    test: user 数据库操作测试

---
+++ src/domain/user/repository.rs
@@ -0,0 +1,12 @@
+use crate::domain::user::error::UserError;
+use crate::domain::user::model::entity::User;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait UserRepository: Send + Sync {
+    async fn select_by_id(&self, id: i64) -> Result<Option<User>, UserError>;
+    async fn select_by_username(&self, username: &str) -> Result<Option<User>, UserError>;
+    async fn insert(&self, username: &str, pwd_hash: &str) -> Result<i64, UserError>;
+    async fn update(&self, user: &User) -> Result<(), UserError>;
+    async fn delete(&self, user_id: i64) -> Result<(), UserError>;
+}
