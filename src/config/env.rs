b86bb8521dfa46cb4cadae3a30c75b9603a54cf5
HLfromZ <hl.1012@icloud.com>
2026-03-12 08:46:43 +0800

    config: 引入axum

--- src/config/env.rs
+++ src/config/env.rs
@@ -5,6 +5,7 @@ pub struct Config {
     pub run_mode: String,
     pub log_level: String,
     pub database_url: String,
+    pub server_port: u16,
 }
 
 pub static CONFIG: OnceLock<Config> = OnceLock::new();
@@ -29,11 +30,13 @@ impl Config {
                 std::process::exit(1);
             }
         };
+        let server_port = env::var("SERVER_PORT").unwrap_or_else(|_| "80".to_string());
 
         let config = Config {
             run_mode,
             log_level,
             database_url,
+            server_port: server_port.parse().unwrap(),
         };
 
         if CONFIG.set(config).is_err() {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::Router;
 use money::config::env::Config;
 use money::config::{db, log};
 use tracing::{error, info};
@@ -18,4 +19,18 @@ async fn main() {
             std::process::exit(-1);
         }
     };
+
+    let addr = format!("0.0.0.0:{}", config.server_port);
+    let listener = match tokio::net::TcpListener::bind(&addr).await {
+        Ok(listener) => listener,
+        Err(_) => {
+            error!("❌ 绑定端口失败");
+            std::process::exit(-1);
+        }
+    };
+
+    info!("🟢 服务启动中, 端口: {}", config.server_port);
+    axum::serve(listener, Router::new())
+        .await
+        .expect("❌ 服务启动失败");
 }
